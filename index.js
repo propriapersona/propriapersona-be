9a4c6e5b326d8adce0e32ef4e5763de13cfe3584
Patrick Shushereba <pshushereba@gmail.com>
2020-06-13 17:36:20 -0700

    Added users table.

--- index.js
+++ index.js
@@ -4,3 +4,20 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+// yarn init --yes
+// fix package json
+// add deps
+// make index.js
+// make server.js
+// knex init
+// fix knexfile
+// knex migrate:make first
+// write migration
+// knex migrate:latest
+// knex seed:make 00-cleanup
+// etc etc
+// knex seed:run
+// make dbconfig
+// make helpers
+// make endpoints
---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: "127.0.0.1",
+      user: "patrick",
+      password: "Getting started",
+      database: "propriapersona",
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+  // staging: {
+  //   client: "postgresql",
+  //   connection: {
+  //     database: "my_db",
+  //     user: "username",
+  //     password: "password",
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  //   migrations: {
+  //     tableName: "knex_migrations",
+  //   },
+  // },
+
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+};
