ec768baeebf209a1d85583a5fa42c631b7f4be91
Patrick Shushereba <pshushereba@gmail.com>
2020-09-12 16:54:28 -0700

    Commented out unfinished routes to fix heroku deploy.

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -11,24 +11,22 @@ router.post("/", (req, res) => {
       res.status(201).json(newAccount);
     })
     .catch((err) => {
-      res
-        .status(500)
-        .json({
-          message: "There was an error adding information to your account",
-        });
+      res.status(500).json({
+        message: "There was an error adding information to your account",
+      });
     });
 });
 
-router.get("/", (req, res) => {
-  // Need to figure out if I want to add the username to the route.
-})
+// router.get("/", (req, res) => {
+//   // Need to figure out if I want to add the username to the route.
+// })
 
-router.put("/", (req, res) => {
-  // To edit account information
-})
+// router.put("/", (req, res) => {
+//   // To edit account information
+// })
 
-router.delete("/" (req, res) => {
-  // For deleting the entire record. Think if that is a good idea. Might re-visit making more of the fields for the table nullable.
-})
+// router.delete("/" (req, res) => {
+//   // For deleting the entire record. Think if that is a good idea. Might re-visit making more of the fields for the table nullable.
+// })
 
 module.exports = router;
