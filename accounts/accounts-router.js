344382aec4594f13043cb6fcc86e6c01d758e719
Patrick Shushereba <pshushereba@gmail.com>
2020-09-11 17:28:49 -0700

    Pseudocode for accounts routes.

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -19,4 +19,16 @@ router.post("/", (req, res) => {
     });
 });
 
+router.get("/", (req, res) => {
+  // Need to figure out if I want to add the username to the route.
+})
+
+router.put("/", (req, res) => {
+  // To edit account information
+})
+
+router.delete("/" (req, res) => {
+  // For deleting the entire record. Think if that is a good idea. Might re-visit making more of the fields for the table nullable.
+})
+
 module.exports = router;
