87bef4df4ace3a132012b7ff07173ed02991505a
Patrick Shushereba <pshushereba@gmail.com>
2020-10-05 16:34:00 -0700

    Created test for server.js.

--- api/server.spec.js
+++ api/server.spec.js
@@ -1,2 +1,12 @@
 const request = require("supertest");
 const server = require("./server.js");
+const db = require("../data/db-config.js");
+const { intersect } = require("../data/db-config.js");
+const { expectCt } = require("helmet");
+
+describe("GET /", () => {
+  it("should return 200 OK", async () => {
+    const res = await request(server).get("/");
+    expect(res.status).toBe(200);
+  });
+});
