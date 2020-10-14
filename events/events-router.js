ec47a8b1365ca4f6a7d99fd9f2878668b4a4f4af
Patrick Shushereba <pshushereba@gmail.com>
2020-10-14 16:00:01 -0700

    Finished basic CRUD routes for events.

--- events/events-router.js
+++ events/events-router.js
@@ -27,15 +27,47 @@ router.get("/:username", (req, res) => {
           res.status(200).json(events);
         })
         .catch((err) => {
-          res
-            .status(500)
-            .json({
-              message:
-                "There was an error retrieving the events from the database.",
-            });
+          res.status(500).json({
+            message:
+              "There was an error retrieving the events from the database.",
+          });
         });
     })
     .catch((err) => {
       res.status(500).json({ message: "Could not find user in the database." });
     });
 });
+
+router.put("/:id", (req, res) => {
+  const updatedEvent = req.body;
+
+  Events.updateEvent(req.params.id, updatedEvent)
+    .then((event) => {
+      res.status(201).json(event);
+    })
+    .catch((err) => {
+      res
+        .status(500)
+        .json({ message: "There was an error updating the event." });
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  Events.deleteEvent(req.params.id)
+    .then((deletedEvent) => {
+      if (deletedEvent) {
+        res
+          .status(200)
+          .json({ message: "The event was successfully deleted." });
+      } else {
+        res
+          .status(404)
+          .json({ message: "An event with that ID could not be found." });
+      }
+    })
+    .catch((err) => {
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
